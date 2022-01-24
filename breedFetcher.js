125d0bd132d1ea71bdd538a30f15de329e250e31
Joseph McGregor <joseph00@my.yorku.ca>
2022-01-24 09:58:13 -0500

    node app which returns details about cat breeds

---
+++ breedFetcher.js
@@ -0,0 +1,51 @@
+const request = require('request');
+
+// API Url to search for Cat Breeds
+//  https://api.thecatapi.com/v1/breeds/search?q=[catBreed]
+if (process.argv.length > 2) {
+  process.argv.slice(2).forEach(breed => {
+
+    const options = {
+      url: `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+      headers: {
+        'x-api-key': '7e2cb31f-5f9d-4b4b-b872-dd2b2b7a2b9a'
+      }
+    };
+
+    request(options, (err, resp, body) => {
+      if (!err) {
+        if (resp.statusCode !== 200) {
+          console.log(`Could not connect to the server. statusCode ${resp.statusCode}`, resp);
+        } else {
+          if (body) {
+            body = JSON.parse(body);
+            if (body.length) {
+              body.forEach(entry => {
+                if (entry.description) {
+                  console.log(`\n${breed} Info:`);
+                  console.log(entry.description);
+                } else {
+                  console.log(`No description for this breed, ${breed}, is provided.`);
+                }
+              });
+            } else {
+              console.log(`Could not find that breed, ${breed}.`);
+            }
+          } else {
+            console.log(`Recieved empty response from server`);
+          }
+        }
+      } else {
+        console.log(`Could not fetch info about '${breed}' cat breed from the server.`, err);
+      }
+    });
+  });
+} else {
+  console.log("Expected");
+  console.log("node breedFetcher '[breed1]' '[breed2]'  '[...]'\r\n\r\n\r\n");
+
+  console.log("Help: Try the following command");
+  console.log("node breedFetcher Siberian Chartreux");
+}
+
+
